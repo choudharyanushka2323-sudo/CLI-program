3fa3165d263debae0e2560a163a9c4333376a231
Anushka Choudhary <choudharyanushka2323@gmsil.com>
2026-09-01 21:12:06 +0530

    Initial commit

---
+++ life.py
@@ -0,0 +1,25 @@
+# taking name 
+name = input ( " what's your name ? :")
+print(" hello " , name )
+
+# gender 
+gender = input ( " specify your gender : ")
+print( " you are " + gender )
+
+# your profession
+profession = input ( " what do you do ? : ")
+print( " so , you are  " + profession )
+
+# year old 
+age = input ( " how old are you ? : ")
+print( " so , you are  " + age )
+
+# your country 
+country = input ( " where are you from ? ")
+
+# taking all together 
+print( " hello " + name )
+print( " you are " + gender )
+print( " so , you are  " + profession )
+print( " and you are  " + age )
+print( " you are from " + country )
